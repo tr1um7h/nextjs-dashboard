14cbdf7cf612bb4a86401aa394ddd5cf19e72cb5
tr1um7h <tr1um7h@gmail.com>
2026-02-14 14:18:34 +0800

    Lesson 8,9: rendering, streaming

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,37 @@
+// import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import { lusitana } from '@/app/ui/fonts';
+import { CardSkeleton, InvoiceSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+
+export default async function Page() {
+
+  /* Deciding where to place your Suspense boundaries
+  You could stream the whole page like we did with loading.tsx... but that may lead to a longer loading time if one of the components has a slow data fetch.
+  You could stream every component individually... but that may lead to UI popping into the screen as it becomes ready.
+  You could also create a staggered effect by streaming page sections. But you'll need to create wrapper components.
+  */
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<InvoiceSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+
+      </div>
+    </main>
+  );
+}
